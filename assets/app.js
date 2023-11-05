2e0dffd8fab0aee4a2114f79f27c5d62a3079070
Kamil Raza <m.kamilrazabhatti@gmail.com>
2023-11-05 21:29:07 +0500

    hero added

--- assets/app.js
+++ assets/app.js
@@ -10,39 +10,37 @@ setTimeout(() => {
 
 const sideBar = document.getElementById("sidebar");
 const navBar = document.getElementById("navbar");
-const socialLink1 = document.getElementById("social-link1")
-const socialLink2 = document.getElementById("social-link2")
-const socialLink3 = document.getElementById("social-link3")
-const socialLink4 = document.getElementById("social-link4")
-const socialLinksNavbar = [socialLink1, socialLink2, socialLink3, socialLink4]
-const socialLink1Sidebar = document.getElementById("social-link1-sidebar")
-const socialLink2Sidebar = document.getElementById("social-link2-sidebar")
-const socialLink3Sidebar = document.getElementById("social-link3-sidebar")
-const socialLink4Sidebar = document.getElementById("social-link4-sidebar")
-const socialLinksSidebar = [socialLink1Sidebar, socialLink2Sidebar, socialLink3Sidebar, socialLink4Sidebar]
-
-
+const socialLink1 = document.getElementById("social-link1");
+const socialLink2 = document.getElementById("social-link2");
+const socialLink3 = document.getElementById("social-link3");
+const socialLink4 = document.getElementById("social-link4");
+const socialLinksNavbar = [socialLink1, socialLink2, socialLink3, socialLink4];
+const socialLink1Sidebar = document.getElementById("social-link1-sidebar");
+const socialLink2Sidebar = document.getElementById("social-link2-sidebar");
+const socialLink3Sidebar = document.getElementById("social-link3-sidebar");
+const socialLink4Sidebar = document.getElementById("social-link4-sidebar");
+const socialLinksSidebar = [
+  socialLink1Sidebar,
+  socialLink2Sidebar,
+  socialLink3Sidebar,
+  socialLink4Sidebar,
+];
 
 let isSidebarOpen = false;
 function handleSideaBarOpen() {
+  console.log("sidebar handler");
   if (!isSidebarOpen) {
-    socialLinksNavbar.forEach( (socialLink) => {
-      socialLink.style.scale = "0"
-    })
     setTimeout(() => {
-      socialLinksSidebar.forEach( (socialLink) => {
-        socialLink.style.scale = "1"
-      })
+      socialLinksSidebar.forEach((socialLink) => {
+        socialLink.style.scale = "1";
+      });
     }, 400);
     sideBar.style.left = "0";
     isSidebarOpen = true;
   } else {
-    socialLinksNavbar.forEach( (socialLink) => {
-      socialLink.style.scale = "1"
-    })
-    socialLinksSidebar.forEach( (socialLink) => {
-      socialLink.style.scale = "0"
-    })
+    socialLinksSidebar.forEach((socialLink) => {
+      socialLink.style.scale = "0";
+    });
     sideBar.style.left = "-100%";
     isSidebarOpen = false;
   }
